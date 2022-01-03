080bd3d7dc2bb7e0c95453f7249b4cf57a894ac0
me <amenpa@tutanota.com>
2022-01-03 12:10:18 -0600

    Cleaned up package.json. Config-overrides needed to start app

--- src/index.js
+++ src/index.js
@@ -1,33 +1,36 @@
 import ReactDOM from 'react-dom'
-import React, { useState, useEffect, useRef, Suspense, useMemo, useCallback } from 'react'
+ import React, { useState, useEffect, useRef, Suspense, useMemo, useCallback } from 'react'
 import {
-  VRCanvas,
+  Interactive,
   useXREvent,
-  Hands,
   Select,
   Hover,
   useXR,
-  Interactive,
   RayGrab,
   useHitTest,
   ARCanvas,
-  DefaultXRControllers,
 } from '@react-three/xr'
-// import { OrbitControls, Sky, Text, Plane, Box } from '@react-three/drei'
-import { Box, Sky, Text } from '@react-three/drei'
+
+import { OrbitControls, Sky, Text, Plane, Box } from '@react-three/drei'
 import { useFrame, useThree } from '@react-three/fiber'
 import { Group } from 'three'
 
 function Button(props) {
+  const ref = useRef()
   const [hover, setHover] = useState(false)
   const [color, setColor] = useState(0x123456)
+
+  // ???
+  useHitTest((hit) => {
+    hit.decompose(ref.current.position, ref.current.rotation, ref.current.scale)
+  });
 
   return (
     <Interactive
       onSelect={() => setColor((Math.random() * 0xffffff) | 0)}
       onHover={() => setHover(true)}
       onBlur={() => setHover(false)}>
-      <Box scale={hover ? [1.5, 1.5, 1.5] : [1, 1, 1]} args={[0.2, 0.1, 0.1]} {...props}>
+      <Box ref={ref} scale={hover ? [1.5, 1.5, 1.5] : [1, 1, 1]} args={[0.2, 0.1, 0.1]} {...props}>
         <meshStandardMaterial attach="material" color={color} />
         <Text position={[0, 0, 0.06]} fontSize={0.025} color="#000" anchorX="center" anchorY="middle">
           Hello world!
@@ -37,29 +40,13 @@ function Button(props) {
   )
 }
 
-function HitTestExample() {
-  const ref = useRef()
-
-  useHitTest((hit) => {
-    hit.decompose(ref.current.position, ref.current.rotation, ref.current.scale)
-  })
-
-  return <Box ref={ref} args={[0.1, 0.1, 0.1]} />
-}
-
 function App() {
   return (
     <ARCanvas camera={{ position: [0, 0, 0.1]}}>
       <ambientLight intensity={0.5} />
       <pointLight position={[5, 5, 5]} />
 
-      <Hands
-      // modelLeft={"https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/left-hand-black-webxr-tracking-ready/model.gltf"}
-      // modelRight={"https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/right-hand-black-webxr-tracking-ready/model.gltf"}
-      />
       <Button position={[0, 0, 0]} />
-      <DefaultXRControllers />
-      {/* <HitTestExample /> */}
     </ARCanvas>
   )
 }
